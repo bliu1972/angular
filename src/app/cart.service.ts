8a74302b49217ae3b619e24b7a82901420a91ad6
bliu72 <bliu72@gmail.com>
2024-06-14 18:54:27 -0400

    fixes

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -27,6 +27,7 @@ export class CartService {
   }
 
   getShippingPrices() {
-    return this.http.get<{type: string, price: number}[]> (this.apiUrl + '/assets/shipping.json');
+    // return this.http.get<{type: string, price: number}[]> (this.apiUrl + '/assets/shipping.json');
+    return this.http.get<{type: string, price: number}[]> (this.apiUrl);
   }
 }
