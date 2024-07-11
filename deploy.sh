bcea8517240beaa7a31051fd3a5ae9b09d4885a8
bliu72 <bliu72@gmail.com>
2024-07-10 22:24:23 -0400

    fixes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 git add -u
 git commit -m "fixes"
 git push
-ng deploy --repo=git@github.com:bliu1972/angular.git --name="<gibhub_username>" --email=<your email>
+ng deploy --repo=git@github.com:bliu1972/angular.git --name="bliu1972" --email=bliu1972@gmail.com
